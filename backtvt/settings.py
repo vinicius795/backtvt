aa79f0b19da55e18c3db8b862b35b46a725e1375
vinicius795 <vinicius795@hotmail.com>
2021-02-12 20:28:05 -0300

    remove front

--- backtvt/settings.py
+++ backtvt/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'funcionarios',
     'parametros',
     'relatorios',
-    'front',
 ]
 
 MIDDLEWARE = [
--- backtvt/urls.py
+++ backtvt/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('front.urls')),
     path('api/cte/', include('cte.urls')),
     path('api/funcionarios/', include('funcionarios.urls')),
     path('api/relatorios/', include('relatorios.urls')),
