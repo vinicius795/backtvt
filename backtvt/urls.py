21dfc5214cd1ebcf2434b917369abdfc2b7f9adb
vinicius795 <vinicius795@hotmail.com>
2021-06-08 17:17:02 -0300

    change urls logic

--- backtvt/urls.py
+++ backtvt/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -5,12 +6,26 @@ from rest_framework_simplejwt.views import (
 )
 from users.views import UserAPIView
 
+admin_patterns = [
+    path('', admin.site.urls)
+]
+
+auth_patterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+
+api_url_patterns = [
+    path('cte/', include('cte.urls')),
+    path('funcionarios/', include('funcionarios.urls')),
+    path('relatorios/', include('relatorios.urls')),
+    path('parametros/', include('parametros.urls')),
+    path('user/', UserAPIView.as_view(), name='user'),
+]
+
 urlpatterns = [
-    path('api/cte/', include('cte.urls')),
-    path('api/funcionarios/', include('funcionarios.urls')),
-    path('api/relatorios/', include('relatorios.urls')),
-    path('api/parametros/', include('parametros.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/user/', UserAPIView.as_view(), name='user'),
+    path('login/', include(auth_patterns)),
+    path('admin/', include(admin_patterns)),
+    path('api/', include(api_url_patterns))
+
 ]
