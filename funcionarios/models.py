f32d1c2da4a5b56520d0fbe884827adbdf3661ec
vinicius795 <vinicius795@hotmail.com>
2021-06-08 22:40:05 -0300

    add status to Veiculos

--- funcionarios/models.py
+++ funcionarios/models.py
@@ -42,6 +42,7 @@ class VEICULOS(models.Model):
     REFERENCIA = models.CharField(max_length=255)
     MODELO = models.CharField(max_length=20)
     PLACA = models.CharField(max_length=8, blank=False, unique=True)
+    status = models.BooleanField(default=True)
 
     def __str__(self):
         return self.PLACA
